af5ad594212cd5ddb50ee00d2313fa358cb811e0
ESPIE <remi.espie@etu.umontpellier.fr>
2024-10-12 01:09:16 +0200

    fix: remove unused deps

--- front/src/main.rs
+++ front/src/main.rs
@@ -4,17 +4,14 @@ mod local_storage;
 mod routes;
 mod types;
 
-use std::env::var;
 use crate::routes::Route;
 use crate::types::AppContext;
 use dioxus::prelude::*;
 use dioxus_logger::tracing::{info, Level};
-use dotenvy::dotenv;
 
 fn main() {
     // Init logger
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
-    dotenv().ok();
     info!("starting app");
     launch_web(app);
 }
@@ -22,7 +19,7 @@ fn main() {
 fn app() -> Element {
     use_context_provider(|| {
         Signal::new(AppContext {
-            api_url: var("API_URL").unwrap_or("https://api.autopbf.espie.dev".to_string()),
+            api_url: "https://api.autopbf.espie.dev".to_string(),
         })
     });
 
