25672d85b30cc55d7e90608d5066bc2b8d1058fd
ESPIE <remi.espie@etu.umontpellier.fr>
2024-11-14 16:29:10 +0100

    feat: add renew check on front

--- front/src/components/account_card.rs
+++ front/src/components/account_card.rs
@@ -1,6 +1,6 @@
 use crate::local_storage::use_persistent;
 use crate::routes::Route;
-use crate::types::{AppContext, Config, ParkingSession};
+use crate::types::{AppContext, Config, ParkingSession, RenewSession};
 use chrono::{DateTime, Datelike};
 use chrono_tz::Europe::Paris;
 use dioxus::prelude::*;
@@ -12,6 +12,8 @@ pub(crate) fn AccountCard_comp(account: Config) -> Element {
     let acc = account.clone();
     let mut session = use_signal(ParkingSession::default);
     let mut loading_session = use_signal(|| true);
+    let mut renew_time = use_signal(|| "".to_string());
+    let mut renew_duration = use_signal(|| "".to_string());
     let mut start_time = use_signal(|| "".to_string());
     let mut expiry_time = use_signal(|| "".to_string());
 
@@ -78,6 +80,43 @@ pub(crate) fn AccountCard_comp(account: Config) -> Element {
                         error!("Can't check account {}: {}", account.name, e);
                     }
                 };
+                match client
+                    .get(format!(
+                        "{}/check_renew?name={}",
+                        context.read().api_url,
+                        account.name
+                    ))
+                    .header("authorization", ["Bearer ", bearer.get().as_str()].concat())
+                    .send()
+                    .await
+                {
+                    Ok(res) => match res.text().await {
+                        Ok(json) => match serde_json::from_str::<RenewSession>(&json) {
+                            Ok(sess) => {
+                                match DateTime::parse_from_rfc3339(sess.next_check.as_str()) {
+                                    Ok(time) => {
+                                        let local_time = time.with_timezone(&Paris);
+                                        renew_time.set(local_time.format("%H:%M").to_string());
+                                    }
+                                    Err(e) => {
+                                        error!("Failed to parse start time: {}", e);
+                                        return;
+                                    }
+                                }
+                                renew_duration.set(sess.duration.to_string());
+                            },
+                            Err(e) => {
+                                error!("Failed to parse renew session: {}", e);
+                            }
+                        },
+                        Err(e) => {
+                            error!("Can't check renew account {}: {}", account.name, e);
+                        }
+                    },
+                    Err(e) => {
+                        error!("Can't check renew account {}: {}", account.name, e);
+                    }
+                }
                 loading_session.set(false);
             }
         }
@@ -113,6 +152,8 @@ pub(crate) fn AccountCard_comp(account: Config) -> Element {
                                 p { class: "title is-4 is-spaced", "Session" }
                                 p { class: "subtitle", "Start: {start_time}" }
                                 p { class: "subtitle has-text-danger", "End: {expiry_time}" }
+                                p { class: "subtitle", "Next renew: {renew_time}" }
+                                p { class: "subtitle", "For at least: {renew_duration}min" }
                         }
                         }
                     }
